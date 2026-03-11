c60b6a312ec2e77279c6fc4462ea66a316142acb
borared <boraofficial76@gmail.com>
2026-03-12 00:55:57 +0700

    Install BrowserRouter for routing and apply in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Navbar from "./components/global/Nabar";
+import Navbar from "./components/global/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <section className="min-h-screen flex items-center justify-center">
+        <img 
+          src="https://res.cloudinary.com/dicrvjstp/image/upload/v1773250810/rbr_lfofek.png" 
+          alt="RBR - Software Engineer"
+          className="max-w-4xl w-full h-auto"
+        />
+      </section>
     </>
   )
 }
