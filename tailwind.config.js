9e40045b08aebf07d23168c19ba6835ce034b469
borared <boraofficial76@gmail.com>
2026-03-11 23:42:49 +0700

    add animation liguid in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        liquid: {
+          "0%": { transform: "scale(1)" },
+          "40%": { transform: "scale(1.15,0.9)" },
+          "60%": { transform: "scale(0.95,1.1)" },
+          "100%": { transform: "scale(1.05)" },
+        },
+      },
+      animation: {
+        liquid: "liquid 0.45s ease-out forwards",
+      },
+    },
   },
   plugins: [],
-}
+};
