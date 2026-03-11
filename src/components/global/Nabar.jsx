e8d3dba05a3e9156ecee31c36eebbadc9ca33179
borared <boraofficial76@gmail.com>
2026-03-11 23:41:11 +0700

    Change size of hamburger to be smaller

--- src/components/global/Nabar.jsx
+++ src/components/global/Nabar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-10 py-6 bg-[#efefef]">
+    <nav className="flex items-center justify-between px-10 py-6 ">
       
       {/* Logo */}
       <div className="text-2xl font-extrabold tracking-tight">
@@ -13,14 +13,14 @@ const Navbar = () => {
       <div className="relative flex items-center justify-center">
 
         {/* dot */}
-        <span className="absolute -left-2 bottom-1 w-2.5 h-2.5 bg-black rounded-full"></span>
+        <span className="absolute -left-2 -bottom-1 w-2.5 h-2.5 bg-black rounded-full"></span>
 
         {/* circle */}
-        <button className="w-16 h-16 bg-black rounded-full flex items-center justify-center">
-          <div className="flex flex-col gap-1.5">
-            <span className="block w-7 h-[3px] bg-white rounded"></span>
-            <span className="block w-7 h-[3px] bg-white rounded"></span>
-            <span className="block w-7 h-[3px] bg-white rounded"></span>
+        <button className="w-12 h-12 bg-black rounded-full flex items-center justify-center">
+          <div className="flex flex-col gap-1">
+            <span className="block w-4 h-[2px] bg-white"></span>
+            <span className="block w-4 h-[2px] bg-white"></span>
+            <span className="block w-4 h-[2px] bg-white"></span>
           </div>
         </button>
       </div>
