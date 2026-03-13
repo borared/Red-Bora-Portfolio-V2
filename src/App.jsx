83d75230f7dec9b3799eea7d9b418325b7cc6f15
borared <boraofficial76@gmail.com>
2026-03-13 23:38:50 +0700

    Added draggable github into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/global/Navbar";
 import Hero from "./components/landingpage/Hero";
 import LandingPage from "./page/landingpage";
+import DraggableGithub from "./components/global/DraggableGithub";
 
 function App() {
   return (
     <BrowserRouter>
       {/* Navbar stays outside Routes so it shows up on every page */}
       <Navbar />
+      <DraggableGithub />
 
       <Routes>
         {/* You must wrap your component in a Route tag */}
@@ -17,7 +19,6 @@ function App() {
         {/* Example of another route */}
         {/* <Route path="/about" element={<About />} /> */}
       </Routes>
-      <section className="min-h-screen" />
     </BrowserRouter>
   );
 }
