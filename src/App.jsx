f090c2c9fa994364ef21e5c366dc894783f42286
borared <boraofficial76@gmail.com>
2026-03-13 22:12:07 +0700

    Fixed problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/global/Navbar";
 import Hero from "./components/Hero";
-import LandingPage from "./page/landingpage";
+import LandingPage from "./page/Landingpage";
 
 function App() {
   return (
     <BrowserRouter>
       {/* Navbar stays outside Routes so it shows up on every page */}
-      <Navbar /> 
-      
+      <Navbar />
+
       <Routes>
         {/* You must wrap your component in a Route tag */}
         <Route path="/" element={<LandingPage />} />
-        
+
         {/* Example of another route */}
         {/* <Route path="/about" element={<About />} /> */}
       </Routes>
-        <section className="min-h-screen" />
+      <section className="min-h-screen" />
     </BrowserRouter>
   );
 }
