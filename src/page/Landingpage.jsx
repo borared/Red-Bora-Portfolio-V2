b1c20ea5d32a07584fe7680942cf850129f3a548
borared <boraofficial76@gmail.com>
2026-03-14 00:34:30 +0700

    imported RecentlyWork.jsx into Landingpage.jsx

--- src/page/Landingpage.jsx
+++ src/page/Landingpage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/landingpage/Hero";
 import Greeting from "../components/landingpage/Greeting";
+import RecentlyWork from "../components/landingpage/RecentlyWork";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen">
       <Hero />
       <Greeting />
+      <RecentlyWork />
     </div>
   )
 }
