eea7f894609fba8282c1cfe4b1e9df357682aa8f
borared <boraofficial76@gmail.com>
2026-03-14 21:19:09 +0700

    fixed bug

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Lottie from "lottie-react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Footer() {
   return (
-    <footer className="relative bg-black text-white py-20 overflow-hidden min-h-[100vh]">
+    <footer className="relative bg-black text-white py-20 overflow-hidden min-h-screen">
       <div className="relative max-w-6xl mx-auto px-6 md:px-12 lg:px-2 flex items-center justify-between mt-72">
         <div className="z-10">
           <p
@@ -18,13 +18,12 @@ export default function Footer() {
           <div className="absolute -right-60 -bottom-50 w-[500px] h-[500px] bg-white rounded-full" />
 
           <div className="relative z-100 w-[300px] h-[300px] -bottom-50">
-            {/* Lottie component removed until valid animationData is provided */}
-            {/* <Lottie
-              animationData={{}} // Replace with your Lottie JSON data
+            <DotLottieReact
+              src="https://lottie.host/4db68bbd-31f6-4cd8-84eb-189de081159a/IGmMCqhzpt.lottie"
               loop
               autoplay
               className="-mt-58 -ml-75 z-0 w-250 flex justify-start"
-            /> */}
+            />
           </div>
         </div>
       </div>
