245ce0cf97bb55da27bce618387a357889add5c6
borared <boraofficial76@gmail.com>
2026-03-14 11:50:09 +0700

    imported DesignWork.jsx

--- src/page/Landingpage.jsx
+++ src/page/Landingpage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/landingpage/Hero";
 import Greeting from "../components/landingpage/Greeting";
 import RecentlyWork from "../components/landingpage/RecentlyWork";
+import DesignWork from "../components/landingpage/DesignWork";
 
 export default function LandingPage() {
   return (
@@ -9,6 +10,7 @@ export default function LandingPage() {
       <Hero />
       <Greeting />
       <RecentlyWork />
+      <DesignWork />
     </div>
   )
 }
