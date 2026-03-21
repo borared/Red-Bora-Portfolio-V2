cb554913952a76f76a206c22c311de64dc6c03cc
borared <boraofficial76@gmail.com>
2026-03-21 19:51:10 +0700

    imported IntroLoader.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/global/Navbar";
 import LandingPage from "./page/Landingpage";
 import DraggableGithub from "./components/global/DraggableGithub";
 import CustomCursor from "./components/global/CustomCursor";
 import Footer from "./components/global/Footer";
+import IntroLoader from "./components/global/IntroLoader";
+
 function App() {
+  const [introVisible, setIntroVisible] = useState(true);
+
   return (
     <BrowserRouter>
-      {/* Navbar stays outside Routes so it shows up on every page */}
       <CustomCursor />
+
+      {/* Intro loading screen – unmounts after animation completes */}
+      {introVisible && (
+        <IntroLoader onComplete={() => setIntroVisible(false)} />
+      )}
+
+      {/* Main site – already rendered but hidden behind the intro overlay */}
       <Navbar />
       <DraggableGithub />
 
       <Routes>
-        {/* You must wrap your component in a Route tag */}
         <Route path="/" element={<LandingPage />} />
-
-        {/* Example of another route */}
-        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
