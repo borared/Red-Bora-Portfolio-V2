2faef7e2a3bd4322e9db7a9c4d3aece8a12df59e
borared <boraofficial76@gmail.com>
2026-04-14 22:38:02 +0700

    Created File: model 2D github in between MoreDetailAbout.jsx and Footer.jsx

---
+++ src/components/landingpage/Model2D.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Model2D() {
+  return (
+    <section className="w-full">
+      <img
+        src="https://res.cloudinary.com/dicrvjstp/image/upload/v1776180835/githubmodel_tjgenb.jpg"
+        alt="Model 2D Characters"
+        className="w-full h-auto object-cover"
+      />
+    </section>
+  );
+}
